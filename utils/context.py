a1be0280639b3f5497964f055bf45733a0139f6d
Laurent <laurent@lagon.tech>
2025-03-10 15:34:46 +0000

    bump to 1.3.0

--- utils/context.py
+++ utils/context.py
@@ -713,7 +713,7 @@ class API:
             password=password,
             priority=priority,
             default_timeout=timeout,
-            user_agent="comfyui-finegrain/1.2.0",
+            user_agent="comfyui-finegrain/1.3.0",
         )
         ctx.run_one_sync(self._login, None)
         ctx.run_one_sync(self._credits, None)
